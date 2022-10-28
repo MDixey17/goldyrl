90bae38bcf0a7b831131507230ff76e2f093d468
Matthew Dixey <dixey001@umn.edu>
2022-10-28 15:54:02 -0500

    added parameters to addMatch - working on database

--- commands/addmatch.js
+++ commands/addmatch.js
@@ -3,12 +3,32 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('addmatch')
-        .setDescription('Add a single match to the database'),
+        .setDescription('Add a single match to the database')
+        .addStringOption(option => 
+            option.setName('team_one')
+                .setDescription('Team 1 in the completed match')
+                .setRequired(true)
+        )
+        .addIntegerOption(option => 
+            option.setName('team_one_score')
+                .setDescription(`Team 1's score in the completed match`)
+                .setRequired(true)
+        )
+        .addStringOption(option => 
+            option.setName('team_two')
+                .setDescription('Team 2 in the completed match')
+                .setRequired(true)
+        )
+        .addIntegerOption(option => 
+            option.setName('team_two_score')
+                .setDescription(`Team 2's score in the completed match`)
+                .setRequired(true)
+        ),
     async execute(interaction) {
         const embed = new EmbedBuilder()
-        .setColor('#32CD32')
-        .setTitle('GoldyRL - Added Match')
-        .setDescription('Successfully added the match information to the database')
+            .setColor('#32CD32')
+            .setTitle('GoldyRL - Added Match')
+            .setDescription('Successfully added the match information to the database')
         await interaction.reply({ embeds: [embed] });
     }
 };
