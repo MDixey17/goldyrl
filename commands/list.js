8dd2e459f92ba72d9ed4c8cae74594677305fd90
Corbin Donner <Corbyjoe87@gmail.com>
2022-10-30 23:54:10 -0500

    Updated /list command and tested in BTS

--- commands/list.js
+++ commands/list.js
@@ -1,10 +1,35 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { Sequelize } = require('sequelize');
+const _MatchData = require('../models/MatchData');
+
+// Connect to the database
+const sequelize = new Sequelize('database', 'user', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+    logging: false,
+    storage: 'matchdata.sqlite',
+});
+
+const MatchData = sequelize.define('match_data', _MatchData);
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('list')
-        .setDescription('To be added'),
+        .setDescription('lists all tags from the database'),
     async execute(interaction) {
-        await interaction.reply(`To be added`);
+        await MatchData.sync();
+        const embed = new EmbedBuilder()
+            .setTitle('GoldyRL - Retrieved List of Teams')
+            .setDescription('The following teams are stored in the database:')
+            .setColor('#32CD32');
+        const teamNames = await MatchData.findAll({attributes: ["team_one", "team_two"]});
+        console.log(teamNames);
+        teamNames.forEach(element => {
+            embed.addFields(
+                {name: String(element.team_one), value: "==========", inline: false},
+                {name: String(element.team_two), value: "==========", inline: false}
+            )
+        });
+        await interaction.reply({ embeds: [embed] });
     }
 };
