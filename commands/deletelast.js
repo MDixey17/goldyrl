8c40165201fc24b249c09e8e73e3b23c51940c32
MDixey17 <matthew_dixey@yahoo.com>
2022-10-31 15:16:57 -0500

    deletelast and matchup implemented

--- commands/deletelast.js
+++ commands/deletelast.js
@@ -1,10 +1,39 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { Sequelize } = require('sequelize');
+const _MatchData = require('../models/MatchData');
+
+// Connect to the database
+const sequelize = new Sequelize('database', 'user', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+    logging: false,
+    storage: 'matchdata.sqlite',
+});
+
+const MatchData = sequelize.define('match_data', _MatchData);
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('deletelast')
-        .setDescription('To be added'),
+        .setDescription('Delete the most recent match for the provided team')
+        .addStringOption(option => 
+            option.setName('team_name')
+                .setDescription('The name of the team to remove the last recorded match')
+                .setRequired(true)
+        ),
     async execute(interaction) {
-        await interaction.reply(`To be added`);
+        await MatchData.sync();
+        
+        const teamName = interaction.options.getString('team_name');
+        const teamEntryToDelete = await MatchData.findOne({ where: Sequelize.or({team_one: teamName}, {team_two: teamName}), order: [ [ 'id', 'DESC'] ] });
+        console.log('Found the data entry', teamEntryToDelete.team_one);
+        const deletedEntries = await MatchData.destroy({ where: {id: teamEntryToDelete.id}});
+
+        const embed = new EmbedBuilder()
+            .setColor('#32CD32')
+            .setTitle('GoldyRL - Deleted Last Match')
+            .setDescription(`Removed last entry for ${teamName}`);
+
+        await interaction.reply({ embeds: [embed] });
     }
 };
--- commands/matchup.js
+++ commands/matchup.js
@@ -1,10 +1,62 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { Sequelize } = require('sequelize');
+const _MatchData = require('../models/MatchData');
+
+// Connect to the database
+const sequelize = new Sequelize('database', 'user', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+    logging: false,
+    storage: 'matchdata.sqlite',
+});
+
+const MatchData = sequelize.define('match_data', _MatchData);
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('matchup')
-        .setDescription('To be added'),
+        .setDescription('Find the matchup history between two teams')
+        .addStringOption(option =>
+            option.setName('team_one')
+                .setDescription('Team 1 in the match')
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option.setName('team_two')
+                .setDescription('Team 2 in the match')
+                .setRequired(true)
+        ),
     async execute(interaction) {
-        await interaction.reply(`To be added`);
+        await MatchData.sync();
+        
+        const teamOne = interaction.options.getString('team_one');
+        const teamTwo = interaction.options.getString('team_two');
+        const matchupDataOne = await MatchData.findAll({ where: {team_one: teamOne, team_two: teamTwo}});
+        const matchupDataTwo = await MatchData.findAll({ where: {team_one: teamTwo, team_two: teamOne}});
+
+        const embed = new EmbedBuilder()
+            .setColor('#32CD32')
+            .setTitle('GoldyRL - Found Matchup Data')
+            .setDescription(`Here is the matchup data found for ${teamOne} vs ${teamTwo}`)
+
+        // Iterate through each matchup list
+        matchupDataOne.forEach(element => {
+            embed.addFields(
+                {name: String(element.team_one) + ' ' + String(element.team_one_score) + ' - ' + String(element.team_two_score) + ' ' + String(element.team_two),
+                    value: String(element.date) + ' - ' + String(element.league),
+                    inline: false
+                }
+            )
+        });
+        matchupDataTwo.forEach(element => {
+            embed.addFields(
+                {name: String(element.team_one) + ' ' + String(element.team_one_score) + ' - ' + String(element.team_two_score) + ' ' + String(element.team_two),
+                    value: String(element.date) + ' - ' + String(element.league),
+                    inline: false
+                }
+            )
+        });
+        
+        await interaction.reply({ embeds: [embed] });
     }
 };
