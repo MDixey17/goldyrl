f41b698528cb6c13705a129ea75ef5af8c50e603
MDixey17 <matthew_dixey@yahoo.com>
2022-11-06 19:20:59 -0600

    Added in permission functionality, waiting reset

--- commands/addmatch.js
+++ commands/addmatch.js
@@ -42,6 +42,14 @@ module.exports = {
                 .setRequired(true)
         ),
     async execute(interaction) {
+        if (!interaction.member.roles.cache.some(role => role.name === "GoldyRL Master" || role.name === "Operations")) {
+            const noPermissionEmbed = new EmbedBuilder()
+                .setColor('#7A0019')
+                .setTitle('GoldyRL - Permission Denied')
+                .setDescription(`You don't have permission to execute /addmatch.\nOnly GoldyRL, Operations, and Admin users can execute this command.`);
+            await interaction.reply( {embeds: [noPermissionEmbed]} );
+            return;
+        }
         await MatchData.sync();
         
         const teamOne = interaction.options.getString('team_one');
