4b1cc109a2dd88d5cc18aa1bbd0ead08a16b64b1
MDixey17 <matthew_dixey@yahoo.com>
2022-11-08 14:32:13 -0600

    Minor change to serverinfo command

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,10 +1,14 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('serverinfo')
         .setDescription('Get server statistics'),
     async execute(interaction) {
-        await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+        const embed = new EmbedBuilder()
+            .setTitle(`GoldyRL - ${interaction.guild.name} Stats`)
+            .setDescription(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`)
+            .setColor("#32CD32");
+        await interaction.reply({ embeds: [embed] });
     }
 };
