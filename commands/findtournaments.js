0e8032df18acecbdf7d254e1d24f26738e0e2b4a
Matthew Dixey <dixey001@umn.edu>
2022-11-19 21:16:23 -0600

    improved /findtournaments

--- commands/findtournaments.js
+++ commands/findtournaments.js
@@ -3,6 +3,13 @@ require('dotenv').config();
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const startggURL = 'https://api.start.gg/gql/alpha';
 
+function sleep(ms) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+        currentDate = Date.now();
+    } while (currentDate - date < ms);
+}
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -38,41 +45,61 @@ module.exports = {
             .setTitle(`GoldyRL - Found Upcoming ${gameTitle} Tournaments`)
             .setDescription(`Found the following ${gameTitle} tournaments that are upcoming`);
 
-        await fetch(startggURL, {
-            method: 'POST',
-                headers: {
-                    'content-type': 'application/json',
-                    'Accept': 'application/json',
-                    Authorization: 'Bearer ' + process.env.START_GG_TOKEN
-                },
-                body: JSON.stringify({
-                    query: `query TournamentsByVideogame($perPage: Int!, $videogameId: ID!) {tournaments(query: {perPage: $perPage page: 1 sortBy: "startAt desc" filter: {past: false videogameIds: [$videogameId]}}) {nodes {id countryCode name numAttendees slug startAt}}}`,
-                    variables: {
-                        perPage: 10,
-                        videogameId: gameID
-                    }
-                })
-        }).then(r => r.json())
-        .then(data => {
-            let tourneys = [...new Set(data.data.tournaments.nodes)];
-            let defaultResponse = 'Not Available';
-            tourneys.forEach(t => {
-                let d = new Date(t.startAt * 1000);
-                var dd = String(d.getDate()).padStart(2, '0');
-                var mm = String(d.getMonth() + 1).padStart(2, '0');
-                var yyyy = String(d.getFullYear());
-                const currentDay = mm + "-" + dd + "-" + yyyy;
-                embed.addFields({
-                    name: `${t.name}`,
-                    value: `ID: ${t.id}\nCountry: ${t.countryCode ?? defaultResponse}\nNumber of Attendees: ${t.numAttendees ?? defaultResponse}\nURL: start.gg/${t.slug}\nStart Date: ${currentDay}`,
-                    inline: false
-                });
-            });
-        })
-        .catch(err => {
-            console.log(err);
-        });
+        let tourneysFound = 0;
+        let page = 1;
+        let today = new Date();
+        while (tourneysFound < 10) { 
+            await fetch(startggURL, {
+                method: 'POST',
+                    headers: {
+                        'content-type': 'application/json',
+                        'Accept': 'application/json',
+                        Authorization: 'Bearer ' + process.env.START_GG_TOKEN
+                    },
+                    body: JSON.stringify({
+                        query: `query TournamentsByVideogame($perPage: Int!, $videogameId: ID!, $pageNumber: Int!) {tournaments(query: {perPage: $perPage page: $pageNumber sortBy: "startAt desc" filter: {past: false videogameIds: [$videogameId]}}) {nodes {id countryCode name numAttendees slug startAt}}}`,
+                        variables: {
+                            perPage: 50,
+                            videogameId: gameID,
+                            pageNumber: page
+                        }
+                    })
+            }).then(r => r.json())
+            .then(data => {
+                let tourneys = [...new Set(data.data.tournaments.nodes)];
+                let defaultResponse = 'Not Available';
+                tourneys.forEach(t => {
+                    if (tourneysFound < 10) {
+                        let d = new Date(t.startAt * 1000);
+                        const diffTime = today - d;
+                        // Make sure the tournaments are in the future
+                        if (diffTime <= 0) {
+                            // Tournament in the future, but make sure not more than 60 days in the future
+                            // Also check to make sure we remove any tournament that has something like "This is a test"
+                            const numDays = Math.ceil(Math.abs(diffTime) / (1000 * 60 * 60 * 24));
+                            if (numDays < 61 && !(t.name.toLowerCase().includes('a test'))) {
+                                var dd = String(d.getDate()).padStart(2, '0');
+                                var mm = String(d.getMonth() + 1).padStart(2, '0');
+                                var yyyy = String(d.getFullYear());
+                                const currentDay = mm + "-" + dd + "-" + yyyy;
+                                embed.addFields({
+                                    name: `${t.name}`,
+                                    value: `ID: ${t.id}\nCountry: ${t.countryCode ?? defaultResponse}\nNumber of Attendees: ${t.numAttendees ?? defaultResponse}\nURL: start.gg/${t.slug}\nStart Date: ${currentDay}`,
+                                    inline: false
+                                });
         
+                                tourneysFound += 1;
+                            }
+                        } 
+                    }
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+            page += 1;
+            sleep(1000);
+        }
         await interaction.editReply( { embeds: [embed] } );
     }
 }
